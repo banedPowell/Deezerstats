50482fc058c42dcfdbd1f129211bf0f4c5f86590
Ilyne <ilynejs@proton.me>
2025-02-22 02:53:48 +0100

    first login / signup pages

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,31 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+	const router = useRouter();
+
+	const user = ref({
+		username: '',
+		password: '',
+	});
+
+	const { login } = useAuthStore();
+	const authStore = storeToRefs(useAuthStore());
+
+	const logToAccount = async () => {
+		await login(user.value);
+
+		if (authStore.isAuthenticated) {
+			await router.push('/');
+		}
+	};
+</script>
 
 <template>
-	<div>
-		<h1>login</h1>
-	</div>
+	<h1>Login</h1>
+
+	<form @submit.prevent="logToAccount">
+		<input v-model="user.username" placeholder="Username" type="text" />
+		<input v-model="user.password" placeholder="Password" type="password" />
+		<button type="submit">Login</button>
+	</form>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,9 +1,31 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+	const router = useRouter();
+
+	const user = ref({
+		username: '',
+		password: '',
+	});
+
+	const { signup } = useAuthStore();
+	const authStore = storeToRefs(useAuthStore());
+
+	const createAccount = async () => {
+		await signup(user.value);
+
+		if (authStore.isAuthenticated) {
+			await router.push('/');
+		}
+	};
+</script>
 
 <template>
-	<div>
-		<h1>Signup</h1>
-	</div>
+	<h1>Signup</h1>
+
+	<form @submit.prevent="createAccount">
+		<input v-model="user.username" placeholder="Username" type="text" />
+		<input v-model="user.password" placeholder="Password" type="password" />
+		<button type="submit">Sign Up</button>
+	</form>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
