376ba5e0bcd3c7e0499f1bd2bb411e119e21a0d2
Ilyne <ilynejs@proton.me>
2025-02-22 01:59:50 +0100

    jwt handler utils

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -1,21 +1,23 @@
 import jwt from 'jsonwebtoken';
 
-const useGenerateJwt = (user: User) => {
+const useGenerateJwt = async (user: User) => {
 	const config = useRuntimeConfig();
 
 	return jwt.sign(
 		{
-			userId: user.id,
+			id: user.id,
 			username: user.username,
 		},
+
 		config.jwtSecret,
+
 		{
 			expiresIn: '30d',
 		},
 	);
 };
 
-const useVerifyJwt = (token: string) => {
+const useVerifyJwt = async (token: string) => {
 	const config = useRuntimeConfig();
 
 	return jwt.verify(token, config.jwtSecret);
