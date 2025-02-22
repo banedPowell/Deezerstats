75f080efa08cb242e2bbe8bb04eae7907c3fcea4
Ilyne <ilynejs@proton.me>
2025-02-22 02:01:30 +0100

    reorganise user table to adapt to randomUserId and remove avatar for now

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,11 +1,10 @@
-import {integer, sqliteTable, text} from 'drizzle-orm/sqlite-core'
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const users = sqliteTable('users', {
-    id: integer('id').primaryKey({autoIncrement: true}),
-    name: text('name').notNull(),
-    username: text('username').notNull().unique(),
-    password: text('password').notNull(),
-    avatar: text('avatar'),
-    createdAt: integer('created_at', {mode: 'timestamp'}).notNull(),
-    updatedAt: integer('updated_at', {mode: 'timestamp'}).notNull()
-})
+	id: text('id').primaryKey().notNull(),
+	name: text('name').notNull(),
+	username: text('username').notNull().unique(),
+	password: text('password').notNull(),
+	createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+	updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull(),
+});
