cdcd9655a4995aa25f17e83fc69285d8e1b696d3
Ilyne <ilynejs@proton.me>
2025-03-03 11:17:50 +0100

    add dashboard page

---
+++ pages/Dashboard/index.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+	definePageMeta({
+		middleware: 'auth-route-middleware',
+	});
+
+	const authStore = useAuthStore();
+
+	const isEmpty = ref(true);
+</script>
+
+<template>
+	<section v-if="isEmpty" class="dashboard empty-dashboard">
+		<h1 class="title">Bienvenue {{ authStore.user.name }}</h1>
+
+		<h2 class="top-title">Vous n'avez pas encore de top</h2>
+
+		<NuxtLink to="/dashboard/imports">
+			<DButton size="small" type="secondary">
+				Importer mes statistiques
+			</DButton>
+		</NuxtLink>
+	</section>
+</template>
+
+<style lang="scss" scoped>
+	h2.top-title {
+		font-family: 'Inter', sans-serif;
+		font-size: 2rem;
+		font-weight: 300;
+	}
+
+	.dashboard {
+		display: flex;
+		flex-direction: column;
+		gap: 30px;
+
+		width: 100%;
+	}
+
+	.import-button {
+		border: 1px dashed $strokes;
+
+		transition: scale 0.1s ease-in-out;
+
+		&:hover {
+			scale: 1.01;
+			transition: scale 0.1s ease-in-out;
+		}
+	}
+
+	.empty-dashboard {
+	}
+</style>
