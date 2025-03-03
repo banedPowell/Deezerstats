d8a11e5f201e1a90db18442db4297dacb662f792
Ilyne <ilynejs@proton.me>
2025-03-03 11:12:50 +0100

    add head configuration: set page title and favicon for Deezerstats

--- app.vue
+++ app.vue
@@ -1,11 +1,23 @@
 <script lang="ts" setup>
 	useAuthStore();
+
+	useHead({
+		title: 'Deezerstats',
+		link: [
+			{
+				rel: 'icon',
+				type: 'image/png',
+				href: '/deezerstats-logo.png',
+			},
+		],
+	});
 </script>
 
 <template>
 	<Html lang="fr">
 		<Body>
 			<NuxtRouteAnnouncer />
+
 			<NuxtLayout>
 				<NuxtPage />
 			</NuxtLayout>
